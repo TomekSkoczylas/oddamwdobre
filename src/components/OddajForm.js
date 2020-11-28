4a360fe8e7180486d6ccaa4ccaf28bca06f93684
TomekSkoczylas <tomasz.m.skoczylas@gmail.com>
2020-11-28 18:23:42 +0100

    Oddaj rzeczy - formularz - panel 3

--- src/components/OddajForm.js
+++ src/components/OddajForm.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 const OddajForm = () => {
-    const [page, setPage] = useState(2);
-
+    const [page, setPage] = useState(3);
+    const [visibility, setVisibility] = useState(false);
+    const [bags, setBags] = useState(null);
+    const [area, setArea] = useState('');
     
     const onChange = event => {
         console.log(event.target.value)
     }
 
+    const onClickBags = n => {
+        setBags(n);
+        setVisibility(false);
+    }
+
+    const onClickArea = a => {
+        setArea(a);
+        setVisibility(false);
+    }
+
     const pageAdd = () => {
+        setVisibility(false);
         if (page < 6) {
             setPage(prevState => prevState + 1)
+            
         }
     }
 
     const pageSubtract = () => {
+        setVisibility(false);
         if (page > 1 ) {
             setPage(prevState => prevState -1)
         }
@@ -97,16 +113,19 @@ const OddajForm = () => {
                     <div className="grey-panel">
                         <div className="second-panel">
                             <div className="second-panel__counter">Krok 2/4</div>
-                            <div className="second-panel__title">Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</div><br/>
-                            <label className="second-panel__label">Liczba 60l worków:
-                                <select className="second-panel__drop-list" value={"1"} placeholder="-wybierz-" name="bags">
-                                    <option className="second-panel__option" value={"1"}>1</option>
-                                    <option className="second-panel__option" value={"2"}>2</option>
-                                    <option className="second-panel__option" value={"3"}>3</option>
-                                    <option className="second-panel__option" value={"4"}>4</option>
-                                    <option className="second-panel__option" value={"5"}>5</option>    
-                                </select>
-                            </label>
+                            <div className="second-panel__title">Podaj liczbę 60l worków, w które spakowałeś/aś rzeczy:</div>
+                            <span className="second-panel__label">Liczba 60l worków:</span>
+                            <span className="second-panel__select" 
+                                onClick={()=> setVisibility(true)}
+                                >{bags ? bags : "- wybierz -"} 
+                                {visibility ? <FiChevronUp className="chevron"/> : <FiChevronDown className="chevron"/>}</span>
+                            <div className={`second-panel__options ${!visibility && "invisible"}`}>
+                                <div className="second-panel__option" onClick={()=>onClickBags(1)}>1</div>
+                                <div className="second-panel__option" onClick={()=>onClickBags(2)}>2</div>
+                                <div className="second-panel__option" onClick={()=>onClickBags(3)}>3</div>
+                                <div className="second-panel__option" onClick={()=>onClickBags(4)}>4</div>
+                                <div className="second-panel__option" onClick={()=>onClickBags(5)}>5</div>
+                            </div>
                             <div className="second-panel__btns">
                                 <div onClick={pageSubtract} className="second-panel__btn wstecz">Wstecz</div>
                                 <div onClick={pageAdd} className="second-panel__btn dalej">Dalej</div>
@@ -127,7 +146,82 @@ const OddajForm = () => {
                         po ich lokalizacjach bądź celu ich pomocy.</div>
                     </div>
                     <div className="grey-panel">
-                        <div className="third-panel"></div>
+                        <div className="third-panel">
+                            <div className="third-panel__counter">Krok 3/4</div>
+                            <div className="third-panel__forms">
+                                <div className="third-panel__area">
+                                    <span className="third-panel__area-label">Lokalizacja:</span><br/>
+                                    <span className="third-panel__area-select" onClick={()=>setVisibility(true)}>
+                                        {area ? area : "-wybierz-"}
+                                        {visibility ? <FiChevronUp className="chevron"/> : <FiChevronDown className="chevron"/>}
+                                    </span>
+                                        <div className={`third-panel__area-options ${!visibility && "invisible"}`}>
+                                            <div className="third-panel__area-option" onClick={()=>onClickArea("Poznań")}>Poznań</div>
+                                            <div className="third-panel__area-option" onClick={()=>onClickArea("Warszawa")}>Warszawa</div>
+                                            <div className="third-panel__area-option" onClick={()=>onClickArea("Kraków")}>Karków</div>
+                                            <div className="third-panel__area-option" onClick={()=>onClickArea("Wrocław")}>Wrocław</div>
+                                            <div className="third-panel__area-option" onClick={()=>onClickArea("Katowice")}>Katowice</div>
+                                        </div> 
+                                </div>
+                                <div className="third-panel__who">
+                                    <span className="third-panel__who-title">Komu chcesz pomóc?</span>
+                                    <div className="third-panel__who-radios" onChange={onChange}>
+                                        <label className="third-panel__who-label">
+                                            <input
+                                                type="radio"
+                                                name="who"
+                                                value="dzieciom"
+                                            />
+                                            <span>dzieciom</span>
+                                        </label>
+                                        <label className="third-panel__who-label">
+                                            <input
+                                                type="radio"
+                                                name="who"
+                                                value="samotnym matkom"
+                                            />
+                                            <span>samotnym matkom</span>
+                                        </label>
+                                        <label className="third-panel__who-label">
+                                            <input
+                                                type="radio"
+                                                name="who"
+                                                value="bezdomnym"
+                                            />
+                                            <span>bezdomnym</span>                                        
+                                        </label>
+                                        <label className="third-panel__who-label">
+                                            <input
+                                                type="radio"
+                                                name="who"
+                                                value="niepełnosprawnym"
+                                            />
+                                            <span>niepełnosprawnym</span>
+                                        </label>
+                                        <label className="third-panel__who-label">
+                                            <input
+                                                type="radio"
+                                                name="who"
+                                                value="osobom starszym"
+                                            />
+                                            <span>osobom starszym</span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="third-panel__org">
+                                    <span className="third-panel__org-title">Wpisz nazwę konkrentej organizacji (opcjonalne)</span><br/>
+                                    <input 
+                                        className="third-panel__org-input"
+                                        type='text'
+                                        name="organization"
+                                    />
+                                </div>
+                            </div>
+                            <div className="third-panel__btns">
+                                <div onClick={pageSubtract} className="third-panel__btn wstecz">Wstecz</div>
+                                <div onClick={pageAdd} className="third-panel__btn dalej">Dalej</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
